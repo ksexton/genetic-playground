3603063aed6f830f7490c06f073d4d23aa5582c5
Kyle Sexton <ks@mocker.org>
2015-06-25 09:52:46 -0500

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to genetic-playground
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/genetic_playground/core_test.clj
@@ -0,0 +1,7 @@
+(ns genetic-playground.core-test
+  (:require [clojure.test :refer :all]
+            [genetic-playground.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
